4a2ff0ff763a72dca3b7e4681e353e55eb42620b
DanieleM <danielemarchio709@gmail.com>
2024-03-13 22:35:48 +0100

    add: correzione esercizio

--- js/app.js
+++ js/app.js
@@ -7,24 +7,25 @@
 let num1
 let num2
 
-for (let i = 1; i <= 100; i++) {
-    num1 = i % 3
-    num2 = i % 5
+for (let i = 0; i < 100; i++) {
+    
+   let num1 = (i + 1) % 3
+   let num2 = (i + 1) % 5
 
-    if(num1 == 0 &&  num2 == 0){
-        console.log('Fizzbuzz',i)
+    if(num1 == 0  &&  num2 == 0){
+        console.log('Fizzbuzz',i + 1)
     }
 
         else if (num1 == 0){
-        console.log('Fizz',i)
+        console.log('Fizz',i + 1)
     }
 
         else if (num2 == 0){
-        console.log('Buzz',i)
+        console.log('Buzz',i + 1)
     }
 
     else{
-        console.log(i)
+        console.log(i + 1)
     }
     
 }
